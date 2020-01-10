8572cf1ef77cfe4ed402b4bad17edd2602133862
shuoyan73 <55779567+shuoyan73@users.noreply.github.com>
2020-01-10 23:17:41 +0800

    Add files via upload

---
+++ Leetcode/283#_Move Zeroes_05170221.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        
+        for i in nums:
+            if i==0:
+                nums.remove(i)
+                nums.append(i)
+                
+
+
+# In[ ]:
+
+
+
+
